4e5b5f9fe93e8df98d8fbf1031f943a7f8039f6b
cjlampwick <joshua.juanchini@gmail.com>
2022-08-19 14:58:26 -0300

    add bootstrap and create header

---
+++ client/src/components/Header.js
@@ -0,0 +1,32 @@
+import React from "react";
+import Button from "react-bootstrap/Button";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../styles/header.css";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+class Header extends React.Component {
+  render() {
+    return (
+      <Row className="app-header justify-content-md-center">
+        <Col className="menu-item" xs lg="2">
+          Noticias
+        </Col>
+        <Col className="menu-item" xs lg="2">
+          Nosotros
+        </Col>
+        <Col className="menu-item main" md="2">
+          <div>Inicio</div>
+        </Col>
+        <Col className="menu-item" xs lg="2">
+          University
+        </Col>
+        <Col className="menu-item" xs lg="2">
+          Coworking
+        </Col>
+      </Row>
+    );
+  }
+}
+
+export default Header;
