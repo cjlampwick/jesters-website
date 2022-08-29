273f737646f259075a7eeb25c02e2b27f465e8cc
Russ <juanchiniezequiel@gmail.com>
2022-08-29 11:47:31 -0300

    login with path0

--- client/src/index.js
+++ client/src/index.js
@@ -3,23 +3,30 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 import App from "./App";
 import News from "./components/Pages/News";
 import Login from "./components/Pages/Login";
 import Us from "./components/Pages/Us";
 
+console.log(
+  process.env.REACT_APP_AUTH0_DOMAIN,
+  process.env.REACT_APP_AUTH0_CLIENT_ID,
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/news" element={<News />} />
-      <Route path="/us" element={<Us />} />
-      <Route path="/login" element={<Login />} />
-    </Routes>
+    <Auth0Provider>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/news" element={<News />} />
+        <Route path="/us" element={<Us />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Auth0Provider>
   </BrowserRouter>
 );
 
